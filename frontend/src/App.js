6396ddcadfd52ccc4b358002afd9b93a17a382c9
hari <harihscn@gmail.com>
2024-08-11 18:42:06 +0530

    https://github.com/HARIHSC/FRONTEND.git

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
 import { Education } from './components/education/education';
 import { Personal } from './components/personal/personal';
+import { Pro } from './components/Props/props';
 import { Skills } from './components/Skills/Skills';
+import { UseStateFunction } from './components/Use/Usestate&useeffects';
 
 
 function App() {
   return (
     <>
-
+{/* 
     <h1>This is my resume</h1>
    <Education />
    <Personal/>
    <Skills/>
+   <UseStateFunction/>
+   <Pro/>
+    */}
+   
+
     </>
   );
 }
